edee366963121514d2a6667efdda30415f54bf43
shiva9533 <129790563+shiva9533@users.noreply.github.com>
2023-10-29 20:26:43 +0530

    learn-shell

--- cart.sh
+++ cart.sh
@@ -2,8 +2,11 @@ cp cart.service /etc/systemd/system/cart.service
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 dnf install nodejs -y
+echo ">>>>>>>>>>>> install complete <<<<<<<<<<<<"
 useradd roboshop
+echo ">>>>>>>>>>>> user added  <<<<<<<<<<<<"
 mkdir /app
+echo ">>>>>>>>>>>> dir created   <<<<<<<<<<<<"
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
