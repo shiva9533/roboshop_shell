f48260235e01543883dda255eba62903276e3acb
shiva9533 <129790563+shiva9533@users.noreply.github.com>
2023-10-29 13:26:48 +0530

    roboshop_script

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,8 @@
 dnf install nginx -y
-rm -rf /usr/share/nginx/html/*
 
+cp  nginx-roboshop.conf  /etc/nginx/default.d/roboshop.conf
+
+rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
